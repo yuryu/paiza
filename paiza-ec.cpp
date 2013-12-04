81b328b31b3afdb548c927046868b8c9d896de3a
Haruka Iwao <haruka@fout.jp>
2013-12-04 17:32:57 +0900

    better sort after reading all rather than keep them sorted

--- paiza-ec.cpp
+++ paiza-ec.cpp
@@ -8,21 +8,6 @@ const int MAX_D = 300;
 
 typedef std::vector<int> intvec_t;
 
-intvec_t read_sorted(const int n)
-{
-	intvec_t result;
-
-	result.reserve(n);
-	for(int i = 0; i < n; ++i){
-		int m;
-		std::cin >> m;
-		result.push_back(m);
-		std::push_heap(result.begin(), result.end(), std::greater<int>());
-	}
-	std::sort_heap(result.begin(), result.end(), std::greater<int>());
-	return result;
-}
-
 intvec_t read_list(const int n)
 {
 	intvec_t result;
@@ -42,14 +27,13 @@ int find_pair(const intvec_t& N, int budget)
 	int r = 0;
 
 	intvec_t::const_iterator n2 = N.end();
-	intvec_t::const_iterator start = std::lower_bound(N.begin(), N.end(), budget, std::greater<int>());
-	for(intvec_t::const_iterator n1 = start;
+	for(intvec_t::const_iterator n1
+			= std::upper_bound(N.begin(), N.end(), budget, std::greater<int>());
 		n1 != N.end();
 		++n1)
 	{
 //		std::cout << "n1: " << *n1 << std::endl;
-
-		if( (budget - r) <= (budget - (*n1 + *start) ) ) break;
+		if( (budget - r) <= (budget - *n1 * 2) ) break;
 		const int remaining = budget - *n1;
 		intvec_t::const_iterator n2c = std::lower_bound(n1, n2, remaining, std::greater<int>());
 		if( n2c == N.end() ) continue;
@@ -61,6 +45,7 @@ int find_pair(const intvec_t& N, int budget)
 		if( (budget - r) > (budget - candidate) )
 		{
 			r = candidate;
+			if( r == budget ) break;
 		}
 	}
 	return r;
@@ -68,12 +53,12 @@ int find_pair(const intvec_t& N, int budget)
 
 int main()
 {
-	intvec_t N, D;
 	int n, d;
 	std::cin >> n >> d;
-	N = read_sorted(n);
-	D = read_list(d);
-
+	intvec_t N = read_list(n);
+	intvec_t D = read_list(d);
+	std::sort(N.begin(), N.end(), std::greater<int>());
+	
 /*
 	std::cout << "N: ";
 	for(int i: N) { std::cout << i << ", "; }
